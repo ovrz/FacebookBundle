9bcacfafa9c2e3a9cd648f507ef523c7b1f680ad
Vincent Bouzeran <vincent.bouzeran@elao.com>
2011-02-10 16:32:16 +0100

    - Fix permissions param name to "req_perms" - Fix permissions option format to string

--- Security/EntryPoint/FacebookAuthenticationEntryPoint.php
+++ Security/EntryPoint/FacebookAuthenticationEntryPoint.php
@@ -44,7 +44,7 @@ class FacebookAuthenticationEntryPoint implements AuthenticationEntryPointInterf
                 'canvas' => $this->options->get('canvas', 0),
                 'display' => $this->options->get('display', 'page'),
                 'fbconnect' => $this->options->get('fbconnect', 1),
-                'permissions' => implode(',', $this->options->get('permissions', array())),
+                'req_perms' => $this->options->get('permissions', ''),
                 'next' => $request->getUriForPath($this->options->get('check_path', '')),
             ))
         );
